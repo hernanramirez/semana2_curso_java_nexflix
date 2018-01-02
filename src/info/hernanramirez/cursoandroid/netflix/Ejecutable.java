293c2dfc4f21083d5501242bcc53322dd651f14d
J. Hernan Ramirez R <hr@hernanramirez.info>
2018-01-02 07:15:19 -0400

    Ejemplo practica evaluada semana 2

--- src/info/hernanramirez/cursoandroid/netflix/Ejecutable.java
+++ src/info/hernanramirez/cursoandroid/netflix/Ejecutable.java
@@ -1,7 +1,5 @@
 package info.hernanramirez.cursoandroid.netflix;
 
-import java.util.ArrayList;
-
 /**
  * El siguiente programa impementa demo con Herencia, poliformismo y interfaces.
  * 
@@ -16,79 +14,75 @@ public class Ejecutable {
 
 	public static void main(String[] args) {
 		/* crear arreglo de peliculas y series */
-		ArrayList<Pelicula> peliculas = new ArrayList<Pelicula>();
-		ArrayList<Serie> series = new ArrayList<Serie>();
-
+		
+		Pelicula[] peliculas = new Pelicula[5];
+		Serie[] series = new Serie[5];
+		
 		int annoPeliculaReciente = 0;
 		int indicePelicula = 0;
 		int maxNroTemporada = 0;
 		int indiceSerie = 0;
-		int contador;
-
+		
 		/* cargar valores de peliculas y */
 
 		// cargar una pelicula como vista y asignarle el tiempo que se ha visto
 
-		Pelicula pelicula = new Pelicula("Coco", "Cine animado 3D", "Lee Unkrich, Adrián Molina", 2016, "120min");
-		pelicula.marcarVisto();
-		pelicula.tiempoVisto = "120 min";
+		peliculas[0] = new Pelicula("Coco", "Cine animado 3D", "Lee Unkrich, Adrián Molina", 2016, "120min");
+		peliculas[0].marcarVisto();
+		peliculas[0].tiempoVisto = "120 min";
 
-		peliculas.add(pelicula);
-		peliculas.add(
-				new Pelicula("El Renacido", "Cine dramático/Suspenso", "Alejandro González Iñárritu", 2016, "2h 36m"));
-		peliculas.add(new Pelicula("Los miserables", "Cine dramático/Cine romántico", "Tom Hooper", 2012, "2h 40m"));
-		peliculas.add(new Pelicula("Arráncame la vida", "Drama/Cine romántico", "Roberto Sneider", 2008, "1h 47m"));
-		peliculas.add(new Pelicula());
+		peliculas[1] = new Pelicula("El Renacido", "Cine dramático/Suspenso", "Alejandro González Iñárritu", 2016, "2h 36m");
+		peliculas[2] = new Pelicula("Los miserables", "Cine dramático/Cine romántico", "Tom Hooper", 2012, "2h 40m");
+		peliculas[3] = new Pelicula("Arráncame la vida", "Drama/Cine romántico", "Roberto Sneider", 2008, "1h 47m");
+		peliculas[4] = new Pelicula();
 
-		Serie serie = new Serie("Sherlock", "Serie de televisión policíaca", "Sue Vertue", "45 min", 3);
-		serie.marcarVisto();
-		serie.tiempoVisto = "45min";
+		series[0] = new Serie("Sherlock", "Serie de televisión policíaca", "Sue Vertue", "45 min", 3);
+		series[0].marcarVisto();
+		series[0].tiempoVisto = "45min";
 
-		series.add(serie);
-		series.add(new Serie("Downton Abbey", "Julian Fellowes"));
-		series.add(new Serie("Merlín", "Drama, Fantasía", "Julian Jones", "50m", 5));
-		series.add(new Serie("Grey's Anatomy", "Drama médico", "Shonda Rhimes", "42m", 12));
-		series.add(new Serie());
+		series[1] = new Serie("Downton Abbey", "Julian Fellowes");
+		series[2] = new Serie("Merlín", "Drama, Fantasía", "Julian Jones", "50m", 5);
+		series[3] = new Serie("Grey's Anatomy", "Drama médico", "Shonda Rhimes", "42m", 12);
+		series[4] = new Serie();
 
 		System.out.println("\n* Lista de las peliculas que se visualizaron");
-		contador = 0;
-		for (Pelicula peliculaObje : peliculas) {
-			if (peliculaObje.esVisto()) {
-				System.out.println(peliculaObje.toString());
-				System.out.println(" ---> " + peliculaObje.tiempoVisto() + "\n");
+		
+		for (int i = 0; i < peliculas.length; i++) {
+			if (peliculas[i].esVisto()) {
+				System.out.println(peliculas[i].toString());
+				System.out.println(" ---> " + peliculas[i].tiempoVisto() + "\n");
 			}
 
 			// tomar el ano mas reciente de las peliculas
-			if (peliculaObje.getAnno() > annoPeliculaReciente) { //
-				annoPeliculaReciente = peliculaObje.getAnno();
-				indicePelicula = contador;
+			if (peliculas[i].getAnno() > annoPeliculaReciente) { //
+				annoPeliculaReciente = peliculas[i].getAnno();
+				indicePelicula = i;
 			}
-			contador++;
 
 		}
 		
 		System.out.println("\n* Pelicula mas reciente");
-		System.out.println(peliculas.get(indicePelicula).toString());
+		System.out.println(peliculas[indicePelicula].toString());
 
 		System.out.println("\n* Lista de las Series que se visualizaron");
-		contador = 0;
-		for (Serie serieObj : series) {
-			if (serieObj.esVisto()) {
-				System.out.println(serieObj.toString());
-				System.out.println(" ---> " + serieObj.tiempoVisto() + "\n");
+		
+		for (int i = 0; i < series.length; i++) {
+			if (series[i].esVisto()) {
+				System.out.println(series[i].toString());
+				System.out.println(" ---> " + series[i].tiempoVisto() + "\n");
 			}
 
 			// tomar el ano mas reciente de las peliculas
-			if (serieObj.getNoDeTemporadas() > maxNroTemporada) { //
-				maxNroTemporada = serieObj.getNoDeTemporadas();
-				indiceSerie = contador;
+			if (series[i].getNoDeTemporadas() > maxNroTemporada) { //
+				maxNroTemporada = series[i].getNoDeTemporadas();
+				indiceSerie = i;
 			}
-			contador++;
+			
 
 		}
 		
 		System.out.println("\n* serie con mas nro de episodios");
-		System.out.println(series.get(indiceSerie).toString());
+		System.out.println(series[indiceSerie].toString());
 
 
 	}
